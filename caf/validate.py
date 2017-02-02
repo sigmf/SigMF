f4d67683778f2aaa7fab57f52bbc2fcf533c7732
Martin Braun <martin.braun@ettus.com>
2017-02-02 14:14:55 +0100

    Adding first pass at API

--- caf/validate.py
+++ caf/validate.py
@@ -2,12 +2,13 @@
 " foo "
 
 from __future__ import print_function
+import re
 import json
 
 def match_type(value, our_type):
     " Checks if value matches our_type "
     return value is None or {
-        'string': lambda x: isinstance(x, str) or isinstance(x, unicode),
+        'string': lambda x: isinstance(x, str) or isinstance(x, unicode), # FIXME make py3k compatible
         'uint': lambda x: isinstance(x, int) and x >= 0,
         'double': lambda x: isinstance(x, float) or isinstance(x, int),
     }[our_type](value)
@@ -30,6 +31,9 @@ def validate_key(data_value, ref_dict, section, key):
             value=data_value,
             key=key,
             type=ref_dict["type"]))
+        return False
+    if "py_re" in ref_dict and not re.match(ref_dict["py_re"], data_value):
+        print("regex fail")
         return False
     return True
 
@@ -79,3 +83,6 @@ if __name__ == "__main__":
     ref_dict_ = json.load(open("schema.json"))
     print(validate(data_dict_, ref_dict_))
 
+				# "py_re": "^(?:[1-9]\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d(?:Z|[+-][01]\d:[0-5]\d)$"
+
+				# "py_re": "\d+\.\d+\.\d+"
