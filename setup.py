83d8e11c8e8d7af11bf6efcd98aa21524000912b
Jacob Gilbert <jacob.gilbert@protonmail.com>
2023-01-06 09:42:53 -0700

    update build config
    will not build 2.x support and updated classifiers and envlist

--- setup.py
+++ setup.py
@@ -22,12 +22,14 @@ setup(
     long_description=longdesc,
     url='https://github.com/gnuradio/SigMF',
     classifiers=[
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     entry_points={
         'console_scripts': [
