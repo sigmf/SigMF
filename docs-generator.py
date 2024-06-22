13ea90a6e35d93a293b5b5752b2521104c8eea00
Marc Lichtman <marclichtman@microsoft.com>
2024-06-21 21:50:58 -0400

    ran black

--- docs-generator.py
+++ docs-generator.py
@@ -58,7 +58,7 @@ def gen_fields(doc, d):
 
 geometry_options = {"tmargin": "1in", "lmargin": "1in", "rmargin": "1in", "bmargin": "1in"}
 doc = Document(geometry_options=geometry_options)
-doc.preamble.append(Command('title', 'SigMF')) # doesn't actually show up anywhere, but was causing a warning when not included
+doc.preamble.append(Command("title", "SigMF"))  # doesn't actually show up anywhere, but was causing a warning when not included
 doc.packages.append(Package("underscore"))  # makes it so _ never means math mode!
 doc.packages.append(Package("xcolor", options=["table"]))  # allows for \rowcolors
 doc.packages.append(Package("listings"))
@@ -216,6 +216,10 @@ with open("main.css", "w") as f:
 
 # Generate HTML from tex with Pandoc
 css_url = "https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
-pandoc_out = subprocess.run(f"pandoc sigmf-spec.tex -f latex -t html -s -o sigmf-spec.html --toc --toc-depth=3 -c {css_url} -c main.css".split(), capture_output = True, text = True)
+pandoc_out = subprocess.run(
+    f"pandoc sigmf-spec.tex -f latex -t html -s -o sigmf-spec.html --toc --toc-depth=3 -c {css_url} -c main.css".split(),
+    capture_output=True,
+    text=True,
+)
 if len(pandoc_out.stderr):
     raise Exception("Pandoc error: " + pandoc_out.stderr)
